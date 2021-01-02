7e193048c323f289ab2d1442b379fa1cc168b6f9
zuotengyui1@gmail.com <ec2-user@ip-172-31-42-39.ec2.internal>
2021-01-02 10:37:31 +0000

    curriculum

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4b16bd2f875cc6d1d794e2dea6bc740ba8f3d2e104cb4d056dfe477f4d98f6ceb39e01379f9ecb2e637e30be61a9be5b7456adb41bc5b61696921d0ff97600f3'
